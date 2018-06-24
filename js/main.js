294fab8743cc3e6e20686596500d2472073c8f9a
Beka Tsalani <beqacalani@gmail.com>
2018-06-24 22:09:36 +0400

    camps

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
 $(document).ready(function () {
-    $("#eventcalendar").simpleCalendar();
+    $("#eventcalendar").simpleCalendar({
+        days: ['კვ', 'ორშაბათი', 'სამშაბათი', 'ოთხშაბათი', 'ხუთშაბათი', 'პარასკევი', 'შაბათი'],
+        fixedStartDay: false
+    });
+    var partners = $('#partners').owlCarousel({
+        loop: true,
+        margin: 30,
+        dots: false,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    });
+    $(".prevpartner").click(function (event) {
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        partners.trigger('prev.owl.carousel');
+    });
+    $(".nextpartner").click(function (event) {
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        partners.trigger('next.owl.carousel');
+    });
+    var mainCarousel = $('#mainCarousel').owlCarousel({
+        loop: true,
+        dots: false,
+        nav: true,
+        items: 1
+    });
 });
 var map;
 function initMap() {
--- js/main.ts
+++ js/main.ts
@@ -1,5 +1,43 @@
 $(document).ready(()=>{
-    $("#eventcalendar").simpleCalendar();
+    $("#eventcalendar").simpleCalendar({
+        days: ['კვ','ორშაბათი','სამშაბათი','ოთხშაბათი','ხუთშაბათი','პარასკევი','შაბათი'],
+        fixedStartDay: false
+    });
+    let partners = $('#partners').owlCarousel({
+        loop:true,
+        margin:30,
+        dots: false,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+    $(".prevpartner").click(event=>{
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        partners.trigger('prev.owl.carousel');
+    });
+    $(".nextpartner").click(event=>{
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        partners.trigger('next.owl.carousel');
+    });
+
+
+    let mainCarousel = $('#mainCarousel').owlCarousel({
+        loop:true,
+        dots: false,
+        nav:true,
+        items: 1
+    });
 });
 
 let map;
