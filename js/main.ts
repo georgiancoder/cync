3009213c68d3093af5a2441a3b8d212d4966c874
beqa <btsalani@emis.ge>
2018-07-09 12:29:03 +0400

    brucianebi

--- js/main.ts
+++ js/main.ts
@@ -113,6 +113,229 @@ $(document).ready(()=>{
         let openText = $(this).parent().find('button.more').data('textopen');
         $(this).parent().find('button.more span').html(openText);
     });
+
+
+
+
+    // brucianebi
+    $("#contrast2").click(function(event) {
+        document.cookie = "contrast=night;expires=7;path=/";
+        $("body").removeClass('sun highcontrast highcontrast2 highcontrast3');
+        $("body").addClass('night');
+        event.preventDefault();
+    });
+
+    $("#contrast1").click(function(event) {
+        document.cookie = "contrast=sun;expires=7;path=/";
+        $("body").removeClass('highcontrast highcontrast2 highcontrast3 night');
+        event.preventDefault();
+    });
+
+    $("#contrast3").click(function(event) {
+        document.cookie = "contrast=highcontrast;expires=7;path=/";
+        $("body").removeClass('sun night highcontrast2 highcontrast3');
+        $("body").addClass('highcontrast');
+        event.preventDefault();
+    });
+
+    $("#contrast4").click(function(event) {
+        document.cookie = "contrast=highcontrast2;expires=7;path=/";
+        $("body").removeClass('sun night highcontrast highcontrast3');
+        $("body").addClass('highcontrast2');
+        event.preventDefault();
+    });
+
+    $("#contrast5").click(function(event) {
+        document.cookie = "contrast=highcontrast3;expires=7;path=/";
+        $("body").removeClass('sun night highcontrast highcontrast2');
+        $("body").addClass('highcontrast3');
+        event.preventDefault();
+    });
+
+
+    $("#scale").click(function(event) {
+        document.cookie = "pagewidth=wide-page;expires=7;path=/";
+        $("body").addClass('wide-page');
+        event.preventDefault();
+    });
+
+    $("#shrink").click(function(event) {
+        document.cookie = "pagewidth=narrow;expires=7;path=/";
+        $("body").removeClass('wide-page');
+        event.preventDefault();
+    });
+
+
+    var k = (document.cookie) ? document.cookie.split(";") : false;
+    // console.log(k);
+    var obj = {};
+    if (k)
+        for (i in k) {
+            if (k[i].split('=')[0] !== 'undefined' || k[i].split('=')[1] !== 'undefined')
+                // console.log('vax bejooo')
+            if (k[i].split('=')[0] !== 'undefined' || k[i].split('=')[1] !== 'undefined')
+                obj[k[i].split('=')[0].trim()] = k[i].split('=')[1].trim();
+        }
+
+
+
+    var count = (obj['pe-font-size']) ? obj['pe-font-size'] : 100;
+    var defaulScale  = 1;
+    var SecFontSize = 10;
+
+
+function cookieData(data){
+    var PCookie = document.cookie;
+    var scale = PCookie.split(';')
+    
+    for(var i = 0; i < scale.length; i++){
+
+        if( scale[i].split('=')[0].trim() == data ){
+            return parseFloat(scale[i].split('=')[1]);
+
+        }
+    }
+}
+
+if(typeof cookieData('scale') != 'undefined'){
+    defaulScale = cookieData('scale');
+    $('html').css({ transform: 'scale('+defaulScale+')' });
+}
+if(typeof cookieData('SecFontSize') != 'undefined'){
+    SecFontSize = cookieData('SecFontSize');
+}
+
+
+
+    $("#decrease").click(function(event) {
+        if (defaulScale == 1 && parseInt(count) > 70) {
+            document.body.className = document.body.className.replace(/fsize.*/, "");
+            count = parseInt(count) - 10;
+            $("body").addClass("fsize" + count);
+            document.cookie = "pe-font-size=" + count + ";expires=7;path=/";
+            decreeseFont();
+        }else if(defaulScale > 0.9){
+            defaulScale -= 0.1;
+            SecFontSize -= 1.5;
+            // console.log('tester')
+            $('html').css({ transform: 'scale('+defaulScale+')' });
+
+                var ww = window.innerWidth;
+                var hw =  $('html').width();
+                window.scrollTo((hw - ww),0)
+
+                document.cookie = "scale="+defaulScale+ ";expires=7;path=/";
+                document.cookie = "SecFontSize="+SecFontSize+ ";expires=7;path=/";
+
+        }
+        event.preventDefault();
+    });
+
+    $("#increase").click(function(event) {
+        if (defaulScale == 1 && parseInt(count) < 130) {
+            $("body").removeClass('fsize' + count);
+            count = parseInt(count) + 10;
+            $("body").addClass("fsize" + count);
+            document.cookie = "pe-font-size=" + count + ";expires=7;path=/";
+            encreaseFont();
+        }else if(defaulScale < 7){
+            defaulScale += 0.1;
+            SecFontSize += 1.5;
+            $('html').css({ transform: 'scale('+defaulScale+')' });
+
+                var ww = window.innerWidth;
+                var hw =  $('html').width();
+                window.scrollTo((hw - ww),0)
+
+                document.cookie = "scale="+defaulScale+ ";expires=7;path=/";
+                document.cookie = "SecFontSize="+SecFontSize+ ";expires=7;path=/";
+        }
+        
+        event.preventDefault();
+    });
+
+    $("#normal").click(function(event) {
+        $("body").removeClass('fsize' + count);
+        document.cookie = "pe-font-size=100;expires=7;path=/";
+        defaulScale = 1;
+        SecFontSize = 10;
+        $('html').css({ transform: 'scale('+defaulScale+')' });
+        document.cookie = "scale="+defaulScale+";expires=7;path=/";
+        document.cookie = "SecFontSize="+SecFontSize+ ";expires=7;path=/";
+        resetFont();
+        event.preventDefault();
+    });
+
+
+
+var fontSize,text,modifyText;
+document.addEventListener('copy', function(e){
+    console.log(defaulScale)
+    if( defaulScale <= 1 ){
+        return false;
+    }
+    fontSize = ( (defaulScale / 1.5) * 50)+'pt';
+    text = document.getSelection().toString();
+    console.log(SecFontSize)
+    modifyText = '<span style="\
+                 font-size:'+SecFontSize+'pt">'
+                 +text+'</span>';
+    e.clipboardData.setData('text/plain', text);
+    e.clipboardData.setData('text/html', modifyText);
+    e.preventDefault();
+});    
+
+    if (count > 100) {
+        //encrease font size
+        for (i = 0; i < ((obj['pe-font-size'] - 100) / 10); i++) {
+            encreaseFont();
+        }
+    } else if (count < 100) {
+
+        //decrease font size
+        for (i = 0; i < ((100 - obj['pe-font-size']) / 10); i++) {
+            // console.log(((100 - obj['pe-font-size']) / 10));
+            decreeseFont();
+        }
+    }
+
+
+    function getClassNames() {
+        $("body").removeAttr('class');
+        $("body").addClass(obj.contrast);
+        $("body").addClass(obj.pagewidth);
+        $("body").addClass("fsize" + obj['pe-font-size']);
+    }
+
+    getClassNames();
+
+
+    function decreeseFont() {
+        $("a, p, li, button, textarea, input, h1, h2, h3, h4, h5, h6, span, label, time").each(function() {
+            $(this).css({ fontSize: (parseInt($(this).css("fontSize")) - 1) + "px" });
+        });
+    }
+
+    function encreaseFont() {
+        $("a, p, li, button, textarea, input, h1, h2, h3, h4, h5, h6, span, label, time").each(function() {
+            $(this).css({ fontSize: (parseInt($(this).css("fontSize")) + 1) + "px" });
+        });
+    }
+
+    function resetFont() {
+        if (parseInt(count) > 100) {
+            $("a, p, li, button, textarea, input, h1, h2, h3, h4, h5, h6, span, label, time").each(function() {
+                $(this).css({ fontSize: (parseInt($(this).css("fontSize")) - ((parseInt(count) - 100) / 10)) + "px" });
+            });
+            count = 100;
+        } else if (parseInt(count) < 100) {
+            $("a, p, li, button, textarea, input, h1, h2, h3, h4, h5, h6, span, label, time").each(function() {
+                $(this).css({ fontSize: (parseInt($(this).css("fontSize")) + ((100 - parseInt(count)) / 10)) + "px" });
+            });
+            count = 100;
+        }
+    }
+
 });
 
 let map;
